bbcdf8659d92385ad32c9708345fbb80b6435c8f
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-02 11:02:13 -0300

    criando meu primeiro projeto vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <OlaMundo/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import OlaMundo from "@/components/OlaMundo.vue"
+
+export default defineComponent({
+  name: 'App',
+  components:{
+    OlaMundo
+  }
+});
+</script>
+
---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Olá Mundo</h1>
+    <h2>Bem vindos ao meu primeiro projeto em VUE 3!!!!</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "OlaMundo"
+
+})
+
+
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
